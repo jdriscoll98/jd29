28658907c2be1de004bb6d70851658e497397623
Jack Driscoll <jdriscoll98@ufl.edu>
2024-03-28 10:37:41 -0400

    Update function to handle edge cases

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,22 @@
 import { BlogHome } from '@/components/blog-home'
-import { NextPage } from 'next'
+import { Metadata, NextPage } from 'next'
 
 interface Props {}
+
+export const metadata: Metadata = {
+  title: "NextGen Marketplaces",
+  description: "Empowering the Next Generation of AI-Enabled Marketplaces",
+  openGraph: {
+    images: [
+      {
+        url: "/header.webp",
+        width: 1200,
+        height: 630,
+        alt: "NextGen Marketplaces",
+      },
+    ],
+  },
+};
 
 const Page: NextPage<Props> = ({}) => {
   return <BlogHome />
